a666b974901bd8e3da150e08e58438953abe9592
Fernando Fagonde <fernandofagonde@gmail.com>
2022-11-22 23:41:24 -0300

    initial commit

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,6 @@
+{{-- This file is used to store sidebar items, inside the Backpack admin panel --}}
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('page') }}"><i class="nav-icon la la-question"></i> Páginas</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('section') }}"><i class="nav-icon la la-question"></i> Seções</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-question"></i> Users</a></li>
